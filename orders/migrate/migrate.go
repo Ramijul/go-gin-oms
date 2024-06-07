b11f05344de311e83b3251bb72921380eaa6a2be
rami <ramizul127@gmail.com>
2024-06-06 22:29:58 -0400

    add default user and product

--- orders/migrate/migrate.go
+++ orders/migrate/migrate.go
@@ -25,5 +25,21 @@ func main() {
 		log.Println("Database connected, inititing migration")
 	}
 
+	//create/update tables
 	session.AutoMigrate(&models.User{}, &models.Product{}, &models.Order{}, &models.OrderDetails{})
+
+	//default user
+	user := models.User{Name: "Ramijul Islam", Email: "ramizul127@gmail.com", PhoneNumber: "3654760064"}
+	result := session.Create(&user)
+	if result.Error != nil {
+		log.Fatal("Unable to create default user", result.Error)
+	}
+
+	//default product
+	product := models.Product{Name: "Huggies Little Movers Baby Diapers", Price: 27.99, InStock: 1000}
+	result = session.Create(&product)
+	if result.Error != nil {
+		log.Fatal("Unable to create default product", result.Error)
+	}
+
 }
