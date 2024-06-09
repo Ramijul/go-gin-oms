9b112d718677b0c0815453b6fad99ff9294eb9b7
rami <ramizul127@gmail.com>
2024-06-09 02:15:13 -0400

    add getMany method

--- orders/productPackage/service.go
+++ orders/productPackage/service.go
@@ -2,6 +2,7 @@ package productPackage
 
 import (
 	"github.com/Ramijul/go-gin-oms/orders/models"
+	"github.com/google/uuid"
 )
 
 type Service struct {
@@ -10,6 +11,15 @@ type Service struct {
 
 func (s *Service) GetAll() (products []*models.Product, err error) {
 	products, err = s.Repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func (s *Service) GetMany(ids []uuid.UUID) (products []*models.Product, err error) {
+	products, err = s.Repository.GetMany(ids)
 	if err != nil {
 		return nil, err
 	}
